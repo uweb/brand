f7ccea85bb12b0df0865de8c87a4b142a56cfef3
Jon Swanson <jswan2@uw.edu>
2015-05-07 14:36:23 -0700

    enabled curated excerpts on pages

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+add_action( 'init', 'add_page_excerpts' );
+
+function add_page_excerpts() {
+  add_post_type_support( 'page', 'excerpt' );
+}
 
 add_action( 'extend_uw_object', function($UW) {
   require( 'setup/uw.brand-scripts.php' );
